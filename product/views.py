f790e83b551fb40977cac32d9fb362bf0945e2d9
thales2911 <savvy.thales@gmail.com>
2024-04-01 09:52:29 +0100

    review endpoints

--- product/views.py
+++ product/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics ,permissions ,status 
-from rest_framework.permissions import AllowAny,IsAdminUser
+from rest_framework.permissions import AllowAny,IsAdminUser,IsAuthenticated
 from .models import *
 from .serializers import *
 from rest_framework.response import Response 
@@ -61,3 +61,35 @@ class UpdateProduct(APIView):
         
         product.delete()
         return Response({'response': 'Product deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+    
+    
+
+class ProductReviewsList(generics.ListAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Retrieve reviews for a specific product.
+        """
+        product = self.get_object_or_404(Product, pk=self.kwargs['pk'])    #we used get_object_or_404 in place of the try except block 
+        
+        return Review.objects.filter(product=product)
+
+class ProductReviewCreate(generics.CreateAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        """
+        Submit a review for a specific product.
+        """
+        product = self.get_object_or_404(Product, pk=self.kwargs['pk'])
+        request.data['product'] = product.id  # Use product.id instead of pk for clarity
+        request.data['user'] = request.user.id
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
